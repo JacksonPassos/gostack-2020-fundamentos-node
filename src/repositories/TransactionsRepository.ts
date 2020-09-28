710d55b8ec47ee57bb6e80c3c5e86d478963d9fb
Jackson Passos <jacksonguitar33@gmail.com>
2020-09-28 14:15:32 -0300

    criando uma transaction

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO{
+  title: string;
+  value, number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -15,14 +22,24 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
     // TODO
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+
+    })
+
+    this.transactions.push(transaction)
+
+    return  transaction
   }
 }
 
